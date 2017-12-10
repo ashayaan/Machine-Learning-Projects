5ce10b6c0dcb1a9a3a70178cdeb52c6ef7aaf458
ashayaan <a.shayaan@gmail.com>
2017-12-10 18:40:57 +0530

    arranging

---
+++ src/document_classification_all.py
@@ -0,0 +1,458 @@
+"""
+======================================================
+Classification of text documents using sparse features
+======================================================
+
+This is an example showing how scikit-learn can be used to classify documents
+by topics using a bag-of-words approach. This example uses a scipy.sparse
+matrix to store the features and demonstrates various classifiers that can
+efficiently handle sparse matrices.
+
+The dataset used in this example is the 20 newsgroups dataset. It will be
+automatically downloaded, then cached.
+
+The bar plot indicates the accuracy, training time (normalized) and test time
+(normalized) of each classifier.
+
+"""
+
+# Author: Peter Prettenhofer <peter.prettenhofer@gmail.com>
+#         Olivier Grisel <olivier.grisel@ensta.org>
+#         Mathieu Blondel <mathieu@mblondel.org>
+#         Lars Buitinck
+# License: BSD 3 clause
+
+#Modified by: Indu Ilanchezian
+#             H. Vijaya Sharvani
+#             Ahmad Shayaan
+
+from __future__ import print_function
+
+import logging
+import string
+import pandas as pd
+import numpy as np
+from optparse import OptionParser
+import sys
+from time import time
+import matplotlib.pyplot as plt
+import pickle
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.linear_model import RidgeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils.extmath import density
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize 
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
+# parse commandline arguments
+op = OptionParser()
+op.add_option("--report",
+              action="store_true", dest="print_report",
+              help="Print a detailed classification report.")
+op.add_option("--chi2_select",
+              action="store", type="int", dest="select_chi2",
+              help="Select some number of features using a chi-squared test")
+op.add_option("--confusion_matrix",
+              action="store_true", dest="print_cm",
+              help="Print the confusion matrix.")
+op.add_option("--top10",
+              action="store_true", dest="print_top10",
+              help="Print ten most discriminative terms per class"
+                   " for every classifier.")
+op.add_option("--all_categories",
+              action="store_true", dest="all_categories",
+              help="Whether to use all categories or not.")
+op.add_option("--use_hashing",
+              action="store_true",
+              help="Use a hashing vectorizer.")
+op.add_option("--n_features",
+              action="store", type=int, default=2 ** 16,
+              help="n_features when using the hashing vectorizer.")
+op.add_option("--filtered",
+              action="store_true",
+              help="Remove newsgroup information that is easily overfit: "
+                   "headers, signatures, and quoting.")
+
+
+def is_interactive():
+    return not hasattr(sys.modules['__main__'], '__file__')
+
+# work-around for Jupyter notebook and IPython console
+argv = [] if is_interactive() else sys.argv[1:]
+(opts, args) = op.parse_args(argv)
+if len(args) > 0:
+    op.error("this script takes no arguments.")
+    sys.exit(1)
+
+print(__doc__)
+op.print_help()
+print()
+
+#############################################################################
+#Stemming
+
+ps = PorterStemmer()
+
+def stemming(sentence):
+    s = sentence.translate(None, string.punctuation)
+    str1 = ''.join([i if ord(i) < 128 else ' ' for i in s]) #selecting only ascii characters
+    words = word_tokenize(str1)
+    words_stemmed = map(lambda x : ps.stem(x), words)
+    return ' '.join([s for s in words_stemmed])
+
+# #############################################################################
+# Load some categories from the training set
+if opts.all_categories:
+    categories = None
+else:
+    categories = [
+        'alt.atheism',
+        'talk.religion.misc',
+        'comp.graphics',
+        'sci.space',
+    ]
+
+if opts.filtered:
+    remove = ('headers', 'footers', 'quotes')
+else:
+    remove = ()
+
+print("Loading 20 newsgroups dataset for categories:")
+print(categories if categories else "all")
+
+#data_train = fetch_20newsgroups(subset='train', categories=categories,shuffle=True, random_state=42,remove=remove)
+
+d = pd.read_csv('../data/kaggle-data-set-labelled2.csv')
+#d = df.dropna().copy()
+freq_values = d['label'].value_counts() 
+#print (freq_values)
+freq_labels = freq_values[freq_values >= 5]
+freq_labels_list  = list(freq_labels.keys())
+#print (freq_labels_list)
+d = d.loc[d['label'].isin(freq_labels_list)]
+#data = d['Name']+d['Summary']+d['Big Description']
+data_raw = d['Name']+d['Summary']+d['Big Description']
+data = data_raw.apply(stemming)
+le = preprocessing.LabelEncoder()
+labels = d['label']
+#print(labels.unique())
+#targets = le.transform(labels)
+
+data_train,data_test,labels_train,labels_test = train_test_split(data,labels,test_size=0.25)
+le.fit(labels)
+np.save('classes.npy', le.classes_)
+y_train = le.transform(labels_train)
+y_test = le.transform(labels_test)
+
+
+#data_train = data[0:200].copy()
+#data_test = data[201:len(d)].copy()
+
+#data_test = fetch_20newsgroups(subset='test', categories=categories,shuffle=True, random_state=42,remove=remove)
+print('data loaded')
+
+# order of labels in `target_names` can be different from `categories`
+target_names = (pd.Series(labels_train).unique())
+#print(le.transform([target_names[0]]))
+#print(len(target_names))
+
+def size_mb(docs):
+    return sum(len(s.encode('utf-8')) for s in docs) / 1e6
+
+#data_train_size_mb = size_mb(data_train)
+#data_test_size_mb = size_mb(data_test)
+
+#print("%d documents - %0.3fMB (training set)" % (
+#    len(data_train), data_train_size_mb))
+#print("%d documents - %0.3fMB (test set)" % (
+#    len(data_test), data_test_size_mb))
+#print("%d categories" % len(categories))
+#print()
+
+
+
+# split a training set and a test set
+#y_train, y_test = targets[0:200].copy(), targets[201:len(d)].copy()
+
+
+print("Extracting features from the training data using a sparse vectorizer")
+t0 = time()
+if opts.use_hashing:
+    vectorizer = HashingVectorizer(stop_words='english', alternate_sign=False,
+                                   n_features=opts.n_features)
+    X_train = vectorizer.transform(data_train)
+else:
+    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                                 stop_words='english')
+    #vectorizer.fit(data)
+    X_train = vectorizer.fit_transform(data_train)
+    filename = 'vectorizer.pkl'
+    pickle.dump(vectorizer, open(filename, 'wb'))
+duration = time() - t0
+#print("done in %fs at %0.3fMB/s" % (duration, data_train_size_mb / duration))
+print("n_samples: %d, n_features: %d" % X_train.shape)
+print()
+
+print("Extracting features from the test data using the same vectorizer")
+t0 = time()
+X_test = vectorizer.transform(data_test)
+duration = time() - t0
+#print("done in %fs at %0.3fMB/s" % (duration, data_test_size_mb / duration))
+print("n_samples: %d, n_features: %d" % X_test.shape)
+print()
+
+# mapping from integer feature name to original token string
+if opts.use_hashing:
+    feature_names = None
+else:
+    feature_names = vectorizer.get_feature_names()
+    #print(feature_names)
+
+if opts.select_chi2:
+    print("Extracting %d best features by a chi-squared test" %
+          opts.select_chi2)
+    t0 = time()
+    ch2 = SelectKBest(chi2, k=opts.select_chi2)
+    X_train = ch2.fit_transform(X_train, y_train)
+    X_test = ch2.transform(X_test)
+    if feature_names:
+        # keep selected feature names
+        feature_names = [feature_names[i] for i
+                         in ch2.get_support(indices=True)]
+    print("done in %fs" % (time() - t0))
+    print()
+
+if feature_names:
+    feature_names = np.asarray(feature_names)
+
+
+def trim(s):
+    """Trim string to fit on terminal (assuming 80-column display)"""
+    return s if len(s) <= 80 else s[:77] + "..."
+
+
+# #############################################################################
+# Benchmark classifiers
+def benchmark(clf,ind):
+    print('_' * 80)
+    print("Training: ")
+    print(clf)
+    t0 = time()
+    clf.fit(X_train, y_train)
+    filename = 'finalized_model_' + ind + '.sav'
+    pickle.dump(clf, open(filename, 'wb'))
+    train_time = time() - t0
+    print("train time: %0.3fs" % train_time)
+
+    t0 = time()
+    pred = clf.predict(X_test)
+    test_time = time() - t0
+    print("test time:  %0.3fs" % test_time)
+
+    score = metrics.accuracy_score(y_test, pred)
+    print("accuracy:   %0.3f" % score)
+
+    #y_score = clf.decision_function(X_test)
+    #fpr["macro"],tpr["macro"] = metrics.roc_curve(y_test.ravel(),y_score.ravel())
+    #roc_auc["macro"] = metrics.auc(fpr,tpr,average="macro")
+
+    if hasattr(clf, 'coef_'):
+        print("dimensionality: %d" % clf.coef_.shape[1])
+        print("density: %f" % density(clf.coef_))
+
+        if opts.print_top10 and feature_names is not None:
+            print("top 10 keywords per class:")
+            for i, label in enumerate(target_names):
+		numerical_label = le.transform([label])
+		i = numerical_label[0]
+                top10 = np.argsort(clf.coef_[i])[-10:]
+                print(trim("%s: %s" % (label, " ".join(feature_names[top10]))))
+        print()
+
+    if opts.print_report:
+        print("classification report:")
+        print(metrics.classification_report(y_test, pred,
+                                            target_names=target_names))
+
+    if opts.print_cm:
+        print("confusion matrix:")
+        print(metrics.confusion_matrix(y_test, pred))
+
+    print()
+    clf_descr = str(clf).split('(')[0]
+    return clf_descr, score, train_time, test_time
+
+
+results = []
+for clf, name in (
+        #(RidgeClassifier(tol=1e-2, solver="lsqr",class_weight="balanced"), "Ridge Classifier"),
+        (Perceptron(n_iter=50,class_weight="balanced"), "Perceptron"),
+        (PassiveAggressiveClassifier(n_iter=50,class_weight="balanced"), "Passive-Aggressive"),
+        (KNeighborsClassifier(n_neighbors=10), "kNN"),
+        (RandomForestClassifier(n_estimators=100,class_weight="balanced"), "Random forest")):
+    print('=' * 80)
+    print(name)
+    results.append(benchmark(clf,name))
+
+for penalty in ["l2", "l1"]:
+    print('=' * 80)
+    print("%s penalty" % penalty.upper())
+    # Train Liblinear model
+    results.append(benchmark(LinearSVC(penalty=penalty, dual=False,
+                                       tol=1e-3,class_weight="balanced"),'liblinear'))
+
+    # Train SGD model
+    results.append(benchmark(SGDClassifier(loss='log',alpha=.0001, n_iter=50,
+                                           penalty=penalty,class_weight="balanced"),'SGD'))
+
+    results.append(benchmark(LogisticRegression(penalty=penalty,class_weight="balanced"),'LR'+penalty))
+
+# Train SGD with Elastic Net penalty
+print('=' * 80)
+print("Elastic-Net penalty")
+results.append(benchmark(SGDClassifier(loss='log',alpha=.0001, n_iter=50,
+                                       penalty="elasticnet",class_weight="balanced"),'SGD_Elastic_net_penalty'))
+
+# Train NearestCentroid without threshold
+print('=' * 80)
+print("NearestCentroid (aka Rocchio classifier)")
+results.append(benchmark(NearestCentroid(),'nearest_centroid'))
+
+# Train sparse Naive Bayes classifiers
+print('=' * 80)
+print("Naive Bayes")
+results.append(benchmark(MultinomialNB(alpha=.01),'MultinomialNB'))
+results.append(benchmark(BernoulliNB(alpha=.01),'BernoulliNB'))
+
+print('=' * 80)
+print("LinearSVC with L1-based feature selection")
+# The smaller C, the stronger the regularization.
+# The more regularization, the more sparsity.
+results.append(benchmark(Pipeline([
+  ('feature_selection', SelectFromModel(LinearSVC(penalty="l1", dual=False,
+                                                  tol=1e-3,class_weight="balanced"))),
+  ('classification', LinearSVC(penalty="l2"))]),'LinearSVC'))
+
+# make some plots
+
+print(results)
+indices = np.arange(len(results))
+
+results = [[x[i] for x in results] for i in range(4)]
+
+clf_names, score, training_time, test_time = results
+training_time = np.array(training_time) / np.max(training_time)
+test_time = np.array(test_time) / np.max(test_time)
+
+plt.figure(figsize=(12, 8))
+plt.title("Score")
+plt.barh(indices, score, .2, label="score", color='navy')
+plt.barh(indices + .3, training_time, .2, label="training time",
+         color='c')
+plt.barh(indices + .6, test_time, .2, label="test time", color='darkorange')
+plt.yticks(())
+plt.legend(loc='best')
+plt.subplots_adjust(left=.25)
+plt.subplots_adjust(top=.95)
+plt.subplots_adjust(bottom=.05)
+
+for i, c in zip(indices, clf_names):
+    plt.text(-.3, i, c)
+
+plt.show()
+
+
+#plt.figure()
+#plt.plot(fpr["macro"], tpr["macro"],label='macro-average ROC curve (area = {0:0.2f})' 
+#	''.format(roc_auc["macro"]),color='navy', linestyle=':', linewidth=4)
+#plt.show()
+
+
+
+
+###########################################################################################################################################################
+
+def top_tfidf_feats(row, features, top_n=25):
+    ''' Get top n tfidf values in row and return them with their corresponding feature names.'''
+    topn_ids = np.argsort(row)[::-1][:top_n]
+    top_feats = [(features[i], row[i]) for i in topn_ids]
+    df = pd.DataFrame(top_feats)
+    df.columns = ['feature', 'tfidf']
+    return df
+
+def top_mean_feats(Xtr, features, grp_ids=None, min_tfidf=0.1, top_n=25):
+    ''' Return the top n features that on average are most important amongst documents in rows
+        indentified by indices in grp_ids. '''
+    if grp_ids:
+        D = Xtr[grp_ids].toarray()
+    else:
+        D = Xtr.toarray()
+
+    D[D < min_tfidf] = 0
+    tfidf_means = np.mean(D, axis=0)
+    return top_tfidf_feats(tfidf_means, features, top_n)
+
+def top_feats_by_class(Xtr, y, features, min_tfidf=0.1, top_n=25):
+    ''' Return a list of dfs, where each df holds top_n features and their mean tfidf value
+        calculated across documents with the same class label. '''
+    dfs = []
+    labels = np.unique(y)
+    for label in labels:
+        ids = np.where(y==label)
+        feats_df = top_mean_feats(Xtr, features, ids, min_tfidf=min_tfidf, top_n=top_n)
+        feats_df.label = label
+        dfs.append(feats_df)
+    return dfs
+
+def plot_tfidf_classfeats_h(dfs):
+    ''' Plot the data frames returned by the function plot_tfidf_classfeats(). '''
+    fig = plt.figure(figsize=(12, 9), facecolor="w")
+    x = np.arange(len(dfs[0]))
+    for i, df in enumerate(dfs):
+        ax = fig.add_subplot(1, len(dfs), i+1)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_frame_on(False)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+        ax.set_xlabel("Mean Tf-Idf Score", labelpad=16, fontsize=14)
+        ax.set_title("label = " + str(df.label), fontsize=16)
+        ax.ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+        ax.barh(x, df.tfidf, align='center', color='#3F5D7D')
+        ax.set_yticks(x)
+        ax.set_ylim([-1, x[-1]+1])
+        yticks = ax.set_yticklabels(df.feature)
+        plt.subplots_adjust(bottom=0.09, right=0.97, left=0.15, top=0.95, wspace=0.52)
+    plt.show()
+
+dfs_train = top_feats_by_class(X_train,labels_train[0:2],feature_names,min_tfidf=0)
+dfs_test = top_feats_by_class(X_test,labels_test[0:2],feature_names,min_tfidf=0)
+plot_tfidf_classfeats_h(dfs_test)
+plot_tfidf_classfeats_h(dfs_train)
+
+
+
+
