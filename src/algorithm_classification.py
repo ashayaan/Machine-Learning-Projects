caf4889543fa87c4458f41d0f501062a07ed769a
ashayaan <a.shayaan@gmail.com>
2018-04-20 12:53:15 +0530

    MLP

--- src/algorithm_classification.py
+++ src/algorithm_classification.py
@@ -8,7 +8,12 @@ from sklearn.neural_network import MLPClassifier
 from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import word_tokenize
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 
 def listify(x):
 	y = x.translate(None, string.punctuation)
@@ -23,7 +28,7 @@ def stemming(sentence):
 	if (type(sentence) is float):
 		sentence = ''
 	s = sentence.translate(None, string.punctuation)
-	str1 = ''.join([i if ord(i) < 128 else ' ' for i in s]) #selecting only ascii characters
+	str1 = ''.join([i if ord(i) < 128 else ' ' for i in s])
 	words = word_tokenize(str1)
 	words_stemmed = map(lambda x : ps.stem(x), words)
 	return ' '.join([s for s in words_stemmed])
@@ -32,18 +37,14 @@ def stemming(sentence):
 
 df = pd.read_csv('../data/new_data/final_data1.csv')
 d_algo = df['Algorithm'].dropna()
-#print d_algo.shape
 d_algo_list = d_algo.apply(listify)
-#print (d_algo_list)
 algo_list = d_algo_list.tolist()
 label_list = flatten(algo_list)
 labels = np.unique(label_list)
-#print(labels)
 
 le = preprocessing.LabelEncoder()
 le.fit(labels)
 encoded_algo = d_algo_list.apply(lambda x : le.transform(x))
-#print (encoded_algo)
 y = MultiLabelBinarizer().fit_transform(encoded_algo)
 
 
@@ -51,17 +52,18 @@ df = df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 
 df_algo = df.loc[d_algo.index]
 data_raw = df_algo['Title']+df_algo['Description']+df_algo['Summary']
 data = data_raw.apply(stemming)
-X_train = loaded_vectorizer.transform(data)
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-clf.fit(X_train, y) 
+data_train,data_test,y_train,y_test = train_test_split(data,y,test_size=0.2)
+X_train = loaded_vectorizer.transform(data_train)
+X_test = loaded_vectorizer.transform(data_test)
+clf = MLPClassifier(activation='relu',solver='sgd', alpha=1e-5, hidden_layer_sizes=(8, 2), random_state=8)
 
-'''
-count = 0
-for x in d_algo_list:
-	count +=1
-	
-	print str(count) + " " + str(le.transform(x))
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+score = metrics.accuracy_score(y_test,pred)
 
 
+clf2 = SGDClassifier(loss='log',alpha=.0001, n_iter=50,class_weight="balanced")
+clf2.fit(X_train, y_train)
+# pred2 = clf2.predict(X_test,y_test)
 
-'''
+print score
