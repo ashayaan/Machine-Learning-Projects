fc894ab9f972afe7037e3a686b4f9026db630f38
ashayaan <a.shayaan@gmail.com>
2018-04-28 12:09:59 +0530

    new code

--- data/new_data/split.py
+++ data/new_data/split.py
@@ -8,22 +8,29 @@ def sperateAlgorithm(x):
 	else:
 		return 0
 
+
+
 if __name__ == '__main__':
 	df = pd.read_csv('final_data1.csv')
-	df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1)
-	df['Label'] = 0
-	# print df['Label']
-	count = 0
-	for x in df['Algorithm']:
-		l = sperateAlgorithm(x)
-		count+=1
-		if l == 0:
-			continue
-		else:
-			for t in l:
-				if t.strip() == "Regression" or t.strip() == "regression" :
-					df.iloc[count-1, df.columns.get_loc('Label')]=1
-					# df.iloc[count-1, 'Label']=1
-
-	print df
+	df = df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1)
+	generate_labels = ['Regression','DataMining','Classification','Clustering','Visualization','TimeSeries','GeospatialAnalysis','NLP','SentimentAnalysis']
+		
+	for label in generate_labels:
+		df[label] = None
 	
+	
+	for label in generate_labels:
+		count = 0
+		for x in df['Algorithm']:
+			l = sperateAlgorithm(x)
+			count+=1
+			if l == 0:
+				continue
+			else:
+				for t in l:
+					if t.strip() == label:
+						df.iloc[count-1, df.columns.get_loc(label)]=1
+					else:
+						df.iloc[count-1, df.columns.get_loc(label)]=0
+
+	df.to_csv('test.csv',index=False)
