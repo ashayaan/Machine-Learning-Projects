e93391e86102c5e321c4a14c7cc561ae83fe196f
induilanchezian <induilanchezian@gmail.com>
2018-05-01 22:07:08 +0530

    Algorithm classification metrics added

--- data/new_data/split.py
+++ data/new_data/split.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import numpy as np
+import string
+#def sperateAlgorithm(x):
+#	if type(x) == type("S"):
+#		return x.strip('[]').split(",")
+#	else:
+#		return 0
 
+def listify(x):
+	if (type(x) == float):
+		return []
+	y = x.translate(None, string.punctuation)
+	return y.split()
+	
 
-def sperateAlgorithm(x):
-	if type(x) == type("S"):
-		return x.strip('[]').split(",")
-	else:
-		return 0
-
-
+flatten = lambda l : [item for sublist in l for item in sublist]
 
 if __name__ == '__main__':
-	df = pd.read_csv('final_data1.csv')
+	df = pd.read_csv('check_data.csv')
 	df = df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1)
-	generate_labels = ['Regression','DataMining','Classification','Clustering','Visualization','TimeSeries','GeospatialAnalysis','NLP','SentimentAnalysis']
+	d_algo = df['Algorithm'].dropna()
+	d_algo_list = d_algo.apply(listify)
+	algo_list = d_algo_list.tolist()
+	label_list = flatten(algo_list)
+	labels = np.unique(label_list)
+	print(labels)
+	generate_labels = ['BigQuery','CausalDiscovery','Classification','Clustering','DataCompression','DataMining','DeepLearning','Detection','Events','FaceDetection','FaceRecognition','FunctionLearning','GeospatialAnalysis','GraphAnalysis','HandwritingRecognition','ImageClassification','NLP','ObjectDetection','ObjectRecognition','Optimization','RecommenderSystems','Regression','RelationalLearning','Segmentation','SentimentAnalysis','SpeechRecognition','Summarization','TimeSeries','TopicModeling','TransferLearning','UnsupervisedClassification','VideoClassification','Visualization']
 		
 	for label in generate_labels:
 		df[label] = None
@@ -22,7 +34,7 @@ if __name__ == '__main__':
 	for label in generate_labels:
 		count = 0
 		for x in df['Algorithm']:
-			l = sperateAlgorithm(x)
+			l = listify(x)
 			count+=1
 			if l == 0:
 				continue
--- src/algorithm_classification.py
+++ src/algorithm_classification.py
@@ -11,6 +11,8 @@ from nltk.tokenize import word_tokenize
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
@@ -18,6 +20,11 @@ from sklearn import metrics
 from collections import Counter
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
+
+from imblearn.combine import SMOTEENN
+from imblearn.under_sampling import ClusterCentroids
+
+import matplotlib.pyplot as plt
 
 def listify(x):
 	y = x.translate(None, string.punctuation)
@@ -37,17 +44,16 @@ def stemming(sentence):
 	words_stemmed = map(lambda x : ps.stem(x), words)
 	return ' '.join([s for s in words_stemmed])
 
-
-
 df = pd.read_csv('../data/new_data/test.csv')
-label_list = df['Classification'].dropna()
-# d_algo_list = d_algo.apply(listify)
-# algo_list = d_algo_list.tolist()
-# label_list = flatten(algo_list)
-# labels = np.unique(label_list)
+label_list = df['GeospatialAnalysis'].dropna()
+d_algo = df['Algorithm'].dropna()
+d_algo_list = d_algo.apply(listify)
+algo_list = d_algo_list.tolist()
+label_list_2 = flatten(algo_list)
+labels = np.unique(label_list)
 # print len(labels)
 
-# print('Training class distributions summary: {}'.format(Counter(label_list)))
+print('Overall class distributions summary: {}'.format(Counter(label_list_2)))
 # print('Test class distributions summary: {}'.format(Counter(y_test)))
 
 # le = preprocessing.LabelEncoder()
@@ -59,21 +65,41 @@ label_list = df['Classification'].dropna()
 
 df = df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1)
 df_algo = df.loc[label_list.index]
+df_algo['Title'].fillna(' ',inplace=True)
+df_algo['Description'].fillna(' ',inplace=True)
+df_algo['Summary'].fillna(' ',inplace=True)
+df_algo['File_Type'].fillna(' ',inplace=True)
 data_raw = df_algo['Title']+df_algo['Description']+df_algo['Summary']+df_algo['File_Type']
 data = data_raw.apply(stemming)
-data_train,data_test,y_train,y_test = train_test_split(data,label_list,test_size=0.2)
-X_train = loaded_vectorizer.transform(data_train)
-X_test = loaded_vectorizer.transform(data_test)
+tfidf = loaded_vectorizer.transform(data)
+X_train,X_test,y_train,y_test = train_test_split(tfidf,label_list,test_size=0.2)
+#X_train = loaded_vectorizer.transform(data_train)
+#X_test = loaded_vectorizer.transform(data_test)
 
-classif = OneVsRestClassifier(SVC(kernel='rbf'))
+#smote_enn = SMOTEENN(random_state=0)
+#X_resampled, y_resampled = smote_enn.fit_sample(X_train, y_train)
+#cc = ClusterCentroids(random_state=0)
+#X_resampled, y_resampled = cc.fit_sample(X_train, y_train)
+#classif = OneVsRestClassifier(SVC(kernel='rbf'))
+#classif = LogisticRegression(class_weight='balanced')
+classif =  MLPClassifier(hidden_layer_sizes=(100,50), max_iter=100, alpha=1e-4,solver='adam', verbose=10, tol=1e-4, random_state=1, learning_rate_init=1e-3)
+#classif = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),algorithm="SAMME",n_estimators=200)
 classif.fit(X_train, y_train)
 pred = classif.predict(X_test)
 score = metrics.accuracy_score(y_test,pred)
-hamming_score = metrics.hamming_loss(y_test,pred)
+cm = metrics.confusion_matrix(y_test,pred)
 # print (le.inverse_transform(m.inverse_transform(pred)))
-print ("Exact matching score: "+str(score))
-print ("Hamming loss: "+str(hamming_score))
+print ("Accuracy: "+str(score))
+print ('Confusion matrix:')
+print (cm)
 
+#### ROC curves ####
+fpr, tpr, thresh = metrics.roc_curve(y_test, pred)
+plt.figure()
+lw=2
+plt.plot(fpr, tpr, color='red')
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.show()
 '''
 clf = MLPClassifier(activation='relu',solver='sgd', alpha=1e-5, hidden_layer_sizes=(100, 50), random_state=8)
 clf.fit(X_train, y_train)
